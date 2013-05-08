6c62a87aa59e6c1b44b931cb5fea0e4a03b57091
Francisco Luis Aranda <faranda@nosolosoftware.biz>
2013-05-08 23:33:06 +0200

    Results store for statistical purposes

--- backend.rb
+++ backend.rb
@@ -30,12 +30,27 @@ helpers do
   def load_game_result( type, subid, result )
     game = YAML.load_file( "games/#{ type }/juego#{ subid }/#{ type }.yml" )
 
+    save_results( type, subid, result )
+
     if result < 0
       game['lose']['image'] = load64( "games/#{ type }/juego#{ subid }/lose.jpg" )
       game['lose']
     else
       game['win']['image'] = load64( "games/#{ type }/juego#{ subid }/win.jpg" )
       game['win']
+    end
+  end
+
+  def save_results( type, subid, result )
+    if File.exists? "games/#{ type }/juego#{ subid }/results.yml"
+      results = YAML.load_file( "games/#{ type }/juego#{ subid }/results.yml" )
+      results["results_#{ ( results.size + 1 ) }"] = result
+    else
+      results = { "results_1" => result }
+    end
+
+    File.open( "games/#{ type }/juego#{ subid }/results.yml", 'w+' ) do |f|
+      f.write( results.to_yaml )
     end
   end
 
